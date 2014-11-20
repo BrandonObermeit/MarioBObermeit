4bee9696565995eb3d7fde90ec90bced33642792
Brandon Obermeit <BladeGX4@gmail.com>
2014-11-20 14:02:33 -0800

    done with 9

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -13,6 +13,7 @@ game.PlayerEntity = me.Entity.extend ({
        }]);
         
         this.renderable.addAnimation("idle", [2]);
+        this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
         
         this.renderable.setCurrentAnimation("idle");
         
@@ -22,11 +23,22 @@ game.PlayerEntity = me.Entity.extend ({
     update: function(delta){
         if(me.input.isKeyPressed("right")){
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            
         }else{
             this.body.vel.x = 0;
         }
         
+        if(this.body.vel.x !== 0){
+            if (!this.renderable.isCurrentAnimation("smallWalk")) {
+                 this.renderable.setCurrentAnimation("smallWalk");
+                 this.renderable.setAnimationFrame();
+            }
+        }else{
+            this.renderable.setCurrentAnimation("idle");
+        }
+        
         this.body.update(delta);
+        this._super(me.Entity, "update" [delta]);
         return true;
     }
     
