b7b9899e9f253e63560ba1cc6f1e1738dba092f0
Brandon Obermeit <BladeGX4@gmail.com>
2014-11-21 14:00:21 -0800

    10

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -28,6 +28,9 @@ game.PlayerEntity = me.Entity.extend ({
             this.body.vel.x = 0;
         }
         
+        this.body.update(delta);
+        me.collision.check(this, true, this.collisionHandler.bind(this), true);
+        
         if(this.body.vel.x !== 0){
             if (!this.renderable.isCurrentAnimation("smallWalk")) {
                  this.renderable.setCurrentAnimation("smallWalk");
@@ -36,11 +39,29 @@ game.PlayerEntity = me.Entity.extend ({
         }else{
             this.renderable.setCurrentAnimation("idle");
         }
-        
-        this.body.update(delta);
+       
         this._super(me.Entity, "update" [delta]);
         return true;
+    },
+    
+    collisionHandler: function(response){
+        
     }
     
+    
+});
+
+game.LevelTrigger = me.Entity.extend({
+    init: function(x, y, settings){
+        this._super(me.Entity, 'init', [x, y, settings]);
+        this.body.onCollision = this.onCollision.bind(this);
+        this.level = settings.level;
+    },
+    
+    onCollision: function(){
+        this.body.setCollosionMask(me.collision.types.NO_OBJECT);
+        me.levelDirector.loadLevel(this.level);
+        me.state.current().resetPlayer();
+    }
     
 });
