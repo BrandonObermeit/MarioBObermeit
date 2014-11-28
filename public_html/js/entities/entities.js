dc638471aede9141278742e451f09f45f730abf6
Brandon Obermeit <BladeGX4@gmail.com>
2014-11-28 15:28:00 -0800

    catch up

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -8,7 +8,7 @@ game.PlayerEntity = me.Entity.extend ({
            width: 128,
            height: 128,
            getShape: function(){
-               return (new me.Rect(0, 0, 128, 128)).toPolygon();
+               return (new me.Rect(0, 0, 30, 128)).toPolygon();
            }
        }]);
         
@@ -18,6 +18,7 @@ game.PlayerEntity = me.Entity.extend ({
         this.renderable.setCurrentAnimation("idle");
         
         this.body.setVelocity(5, 20);
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
     
     update: function(delta){
@@ -56,12 +57,14 @@ game.LevelTrigger = me.Entity.extend({
         this._super(me.Entity, 'init', [x, y, settings]);
         this.body.onCollision = this.onCollision.bind(this);
         this.level = settings.level;
+        this.xspawn = settings.xspawn;
+        this.yspawn = settings.yspawn;
     },
     
     onCollision: function(){
         this.body.setCollosionMask(me.collision.types.NO_OBJECT);
         me.levelDirector.loadLevel(this.level);
-        me.state.current().resetPlayer();
+        me.state.current().resetPlayer(this.xspawn, this.yspawn);
     }
     
 });
