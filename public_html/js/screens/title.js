cfc1f744c4dc5c55f13f6cb814213903261064a8
Brandon Obermeit <BladeGX4@gmail.com>
2014-12-03 13:52:17 -0800

    *

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -22,7 +22,7 @@ game.TitleScreen = me.ScreenObject.extend({
         
 	this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
            if(action === "start"){
-               me.state.chhange(me.state.PLAY);
+               me.state.change(me.state.PLAY);
            } 
         });        
     },
