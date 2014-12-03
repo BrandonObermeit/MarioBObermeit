657020eda500d83a6b7c9c0301767e59d541016f
Brandon Obermeit <BladeGX4@gmail.com>
2014-12-03 08:16:16 -0800

    no

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,7 +3,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		me.game.world.addChild( new me.Sprite (0, 0, me.loader.getImage('title-screen')), 3);
+		me.game.world.addChild( new me.Sprite (0, 0, me.loader.getImage('title-screen')), -10);
                 me.input.bindKey(me.input.KEY.ENTER, "start");
 	
 	me.game.world.addChild(new (me.Renderable.extend ({
@@ -14,6 +14,7 @@ game.TitleScreen = me.ScreenObject.extend({
             
             draw: function(renderer){
                 this.font.draw(renderer.getContext(), "Marioish", 450, 130);
+                this.font.draw(renderer.getContext(), "Press ENTER to play", 250, 530);
             }
             
         })));
@@ -30,5 +31,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 */
 	onDestroyEvent: function() {
 		me.input.unbindKey(me.input.KEY.ENTER);
+                me.event.unsubscribe(this.handler);
 	}
 });
