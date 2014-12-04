6c437eb9eb1aa85acf4959b5dc0113b1ee0621cd
Brandon Obermeit <BladeGX4@gmail.com>
2014-12-04 08:20:56 -0800

    .

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -9,9 +9,11 @@ game.resources = [
          {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
          {name: "mario", type:"image", src: "data/img/player1.png"},
          {name: "title-screen", type:"image", src: "data/img/title-screen.png"},
+         {name: "slime", type:"image", src: "data/img/slime-spritesheet.png"},
          
-	/* Atlases 
-	 * @example
+         
+         
+	/* Atlases * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
 	 */
 		
